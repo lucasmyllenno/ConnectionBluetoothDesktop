4f1201a4a8e82babc8a4fd7e15765c3f6faf5049
Lucas Myllenno Silva Monteiro Lima <lucas.mlima@hotmail.com.br>
2015-09-26 06:15:15 -0300

    Sincronização de servidor e cliente

---
+++ C#/Client.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Windows.Forms;
+using InTheHand.Net.Sockets;
+
+namespace ConsoleAdapterBluetooth
+{
+    class Client
+    {
+		private Guid UUID;
+		private String PIN;
+        private BluetoothAddress bluetoothAddress;
+		private BluetoothClient bluetoothClient;
+		
+		private String stringSend;
+		private String status;
+		
+		public Client()
+		{
+			UUID = new Guid("00000000-0000-0000-0000-000000000000");
+            PIN = "1234";
+			
+			bluetoothAddress = getMacAddress();
+		}
+		
+		public void start(BluetoothDeviceInfo serverToSend)
+		{
+			try
+			{
+				if (serverToSend.Authenticated)
+				{
+					bluetoothClient = new BluetoothClient();
+                    bluetoothClient.Connect(serverToSend.DeviceAddress, UUID);
+					Console.WriteLine("Conexão Estabelecida!");
+
+                    Thread thread = new Thread(new ThreadStart(threadClientBluetooth));
+                    thread.Start();
+				}
+				else
+				{
+                    Console.WriteLine("Servidor Não Autenticado!");
+				}
+			}
+			catch (Exception e)
+			{
+                Console.WriteLine("Erro: " + e.ToString());
+			}
+		}
+		
+		public void threadClientBluetooth()
+		{
+			try
+			{
+				Stream stream = bluetoothClient.GetStream();
+
+                byte[] sent = Encoding.ASCII.GetBytes(stringSend);
+                stream.Write(sent, 0, sent.Length);
+                stream.Flush();
+
+                Console.WriteLine("Mensagem Enviada!");
+			}
+			catch (Exception e)
+			{
+                Console.WriteLine("Erro: " + e.ToString());
+			}
+		}
+		
+		public BluetoothAddress getMacAddress()
+		{
+            BluetoothRadio bluetoothRadio = BluetoothRadio.PrimaryRadio;
+            BluetoothAddress bluetoothAddress = bluetoothRadio.LocalAddress;
+            return bluetoothAddress;
+		}
+		
+		public void close()
+        {
+            bluetoothClient.Close();
+            bluetoothClient.Dispose();
+            Console.WriteLine("Conexão terminada!");
+        }
+		
+		public void setStringSend(String status)
+		{
+			this.stringSend = stringSend;
+		}
+		
+		public String getStringSend()
+		{
+			return stringSend;
+		}
+	}
+}
---
+++ C#/Server.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Windows.Forms;
+using InTheHand.Net.Sockets;
+
+namespace ConsoleAdapterBluetooth
+{
+    class Server
+    {
+        private Guid UUID;
+		private BluetoothListener bluetoothListener;
+        private BluetoothClient bluetoothClient;
+		
+		public Server()
+		{
+			UUID = new Guid("00000000-0000-0000-0000-000000000000");
+		}
+		
+		public void start()
+		{
+            try
+            {
+                bluetoothListener = new BluetoothListener(UUID);
+                bluetoothListener.Start();
+
+                Console.WriteLine("Aguardando Clientes..");
+
+                if (bluetoothListener.Pending())
+                {
+                    Thread thread = new Thread(new ThreadStart(readInfoClient));
+                    thread.Start();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: " + e.ToString());
+            }
+		}
+		
+		public void readInfoClient()
+		{
+            bluetoothClient = bluetoothListener.AcceptBluetoothClient();
+            Console.WriteLine("Cliente Conectado!");
+
+			Stream stream = bluetoothClient.GetStream();
+			
+			while (bluetoothClient.Connected)
+            {
+                try
+                {
+                    byte[] byteReceived = new byte[1024];
+                    int read = stream.Read(byteReceived, 0, byteReceived.Length);
+                    if (read > 0)
+                    {
+                        Console.WriteLine("Messagem Recebida: " + Encoding.ASCII.GetString(byteReceived) + System.Environment.NewLine);
+                    }
+                    stream.Flush();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erro: " + e.ToString());
+                }
+            }
+            stream.Close();
+		}
+
+        public void close()
+        {
+            bluetoothClient.Close();
+            bluetoothClient.Dispose();
+            Console.WriteLine("Conexão terminada!");
+        }
+	}
+}
