f2a997c3c2bff23d09b28af043627e8ef5e48182
Lucas M Lima <lucas.mlima@hotmail.com.br>
2015-09-26 06:22:56 -0300

    Update Server.cs

--- C#/Server.cs
+++ C#/Server.cs
@@ -14,9 +14,9 @@ namespace ConsoleAdapterBluetooth
 {
     class Server
     {
-        private Guid UUID;
+        	private Guid UUID;
 		private BluetoothListener bluetoothListener;
-        private BluetoothClient bluetoothClient;
+	        private BluetoothClient bluetoothClient;
 		
 		public Server()
 		{
@@ -25,57 +25,57 @@ namespace ConsoleAdapterBluetooth
 		
 		public void start()
 		{
-            try
-            {
-                bluetoothListener = new BluetoothListener(UUID);
-                bluetoothListener.Start();
-
-                Console.WriteLine("Aguardando Clientes..");
-
-                if (bluetoothListener.Pending())
-                {
-                    Thread thread = new Thread(new ThreadStart(readInfoClient));
-                    thread.Start();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Erro: " + e.ToString());
-            }
+	            	try
+	            	{
+	                	bluetoothListener = new BluetoothListener(UUID);
+	                	bluetoothListener.Start();
+	
+	                	Console.WriteLine("Aguardando Clientes..");
+	
+	                	if (bluetoothListener.Pending())
+	                	{
+	                		Thread thread = new Thread(new ThreadStart(readInfoClient));
+	                    		thread.Start();
+	                	}
+	            	}
+	            	catch (Exception e)
+	            	{
+	                	Console.WriteLine("Erro: " + e.ToString());
+	            	}
 		}
 		
 		public void readInfoClient()
 		{
-            bluetoothClient = bluetoothListener.AcceptBluetoothClient();
-            Console.WriteLine("Cliente Conectado!");
+            		bluetoothClient = bluetoothListener.AcceptBluetoothClient();
+            		Console.WriteLine("Cliente Conectado!");
 
 			Stream stream = bluetoothClient.GetStream();
 			
 			while (bluetoothClient.Connected)
-            {
-                try
-                {
-                    byte[] byteReceived = new byte[1024];
-                    int read = stream.Read(byteReceived, 0, byteReceived.Length);
-                    if (read > 0)
-                    {
-                        Console.WriteLine("Messagem Recebida: " + Encoding.ASCII.GetString(byteReceived) + System.Environment.NewLine);
-                    }
-                    stream.Flush();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Erro: " + e.ToString());
-                }
-            }
-            stream.Close();
+            		{
+                		try
+		                {
+		                	byte[] byteReceived = new byte[1024];
+		                    	int read = stream.Read(byteReceived, 0, byteReceived.Length);
+		                    	if (read > 0)
+		                    	{
+		                        	Console.WriteLine("Messagem Recebida: " + Encoding.ASCII.GetString(byteReceived) + System.Environment.NewLine);
+		                    	}
+		                    	stream.Flush();
+		                }
+		                catch (Exception e)
+		                {
+		                    	Console.WriteLine("Erro: " + e.ToString());
+		                }
+            		}
+            		stream.Close();
 		}
 
-        public void close()
-        {
-            bluetoothClient.Close();
-            bluetoothClient.Dispose();
-            Console.WriteLine("Conexão terminada!");
-        }
+	        public void close()
+	        {
+	        	bluetoothClient.Close();
+	            	bluetoothClient.Dispose();
+	            	Console.WriteLine("Conexão terminada!");
+	        }
 	}
 }
